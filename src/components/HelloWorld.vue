22fa567a2bf4d9fd1e99650343fb0ac06983ad61
Tonia <antosia535@gmail.com>
2022-07-18 13:05:13 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
